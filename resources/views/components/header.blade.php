33f87fbed5b2de8307fdb65b7deae4632b444b74
Randi Fernandez-Moran <randi.fernandez-moran@hotmail.com>
2025-02-06 13:13:50 +0100

    2

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,5 +1,6 @@
-<header id="header" class="alt">
+<header id="header" class="{{ Route::currentRouteName() == 'home' ? 'alt' : '' }}">
     <span class="logo"><img src="images/logo.svg" alt="" /></span>
+    
     <h1>Stellar</h1>
     <p>Just another free, fully responsive site template<br /> built by <a href="https://twitter.com/ajlkn">@ajlkn</a> for <a href="https://html5up.net">HTML5 UP</a>.</p>
     <x-nav />
